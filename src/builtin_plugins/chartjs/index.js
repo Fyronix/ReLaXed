8b366668d7157b5316866d2f807461d482004b29
Fyronix <86478645+Fyronix@users.noreply.github.com>
2023-03-02 20:56:12 +0100

    Update index.js

--- src/builtin_plugins/chartjs/index.js
+++ src/builtin_plugins/chartjs/index.js
@@ -1,5 +1,5 @@
 const pug = require('pug')
-const fs = require('fs')
+const fs = require('fs').promises
 const path = require('path')
 
 exports.constructor = async function (params) {
@@ -13,28 +13,24 @@ exports.constructor = async function (params) {
   }
 }
 
+const chartjsHandler = async function (chartjsPath, page) {
+  const chartSpec = await fs.readFile(chartjsPath, 'utf8')
+  const html = pug.renderFile(path.join(__dirname, 'template.pug'), { chartSpec })
+  const tempHTML = `${chartjsPath}.htm`
 
-var chartjsHandler = async function (chartjsPath, page) {
-  var chartSpec = fs.readFileSync(chartjsPath, 'utf8')
-  var html = pug.renderFile(path.join(__dirname, 'template.pug'), { chartSpec })
-  var tempHTML = chartjsPath + '.htm'
-
-  fs.writeFileSync(tempHTML, html)
+  await fs.writeFile(tempHTML, html)
   await page.setContent(html)
   await page.waitForFunction(() => window.pngData)
 
   const dataUrl = await page.evaluate(() => window.pngData)
   const { buffer } = parseDataUrl(dataUrl)
-  var pngPath = chartjsPath.substr(0, chartjsPath.length - '.chart.js'.length) + '.png'
-  fs.writeFileSync(pngPath, buffer, 'base64')
+  const { dir, name } = path.parse(chartjsPath)
+  const pngPath = path.join(dir, `${name}.png`)
+  await fs.writeFile(pngPath, buffer, 'base64')
 }
-// Scrape (pull) images from the web
-// from https://intoli.com/blog/saving-images/
-var parseDataUrl = function (dataUrl) {
+
+const parseDataUrl = function (dataUrl) {
   const matches = dataUrl.match(/^data:(.+);base64,(.+)$/)
-  if (matches.length !== 3) {
-    throw new Error('Could not parse data URL.')
-  }
   return {
     mime: matches[1],
     buffer: Buffer.from(matches[2], 'base64')
